c5e29f5abf8a67c900bc23f4f5bba8f68ed61086
Евгений Субочев <panc96@yandex.ru>
2023-02-27 23:29:04 +0300

    test

---
+++ api_yamdb/api_yamdb/models.py
@@ -0,0 +1,9 @@
+from .asgi models.IntegerField(_("
+                                "))
+
+
+test sadf sdaf 
+s defaf 
+models.GenericIPAddressField(_(" s
+daf 
+sd"), protocol="both", unpack_ipv4=False)
