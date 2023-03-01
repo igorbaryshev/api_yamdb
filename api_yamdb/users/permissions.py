95479d826d0eb37b4d1a2f2a1f410155636fa8ea
Igor Baryshev <igoralebar@gmail.com>
2023-03-01 21:02:30 +0300

    remove request.user check

--- api_yamdb/users/permissions.py
+++ api_yamdb/users/permissions.py
@@ -14,10 +14,8 @@ class IsAuthorOrReadOnly(BasePermission):
     def has_object_permission(self, request, view, obj):
         return bool(request.method in SAFE_METHODS
                     or request.user == obj.author
-                    or request.user
-                    and (request.user.role in ['moderator', 'admin']
-                         or request.user.is_staff)
-                    )
+                    or request.user.role in ['moderator', 'admin']
+                    or request.user.is_staff)
 
 
 class IsAdminOrReadOnly(BasePermission):
