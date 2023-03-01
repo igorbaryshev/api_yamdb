c361b063bc699fe73d5f6ae6bc2cf16b43093a39
Дмитрий Губин <gubin-dmitry@mail.ru>
2023-03-01 21:16:42 +0300

    dobavil urls

--- api_yamdb/api/reviews/urls.py
+++ api_yamdb/api/reviews/urls.py
@@ -1,4 +1,17 @@
-from django.urls import include
+from django.urls import include, path
+from rest_framework.routers import DefaultRouter
+
+from reviews.views import CommentViewSet, ReviewViewSet
+
+v1_router = DefaultRouter()
+v1_router.register(r'titles/(?P<titles_id>\d+)/reviews', ReviewViewSet)
+v1_router.register(
+    r'titles/(?P<titles_id>\d+)/reviews/(?P<review_id>\d+)/comments',
+    CommentViewSet,
+    basename='comments'
+)
 
 
-urlpatterns = []
+urlpatterns = [
+    path('v1/', include(v1_router.urls)),
+]
--- api_yamdb/reviews/models.py
+++ api_yamdb/reviews/models.py
@@ -1,14 +1,14 @@
 from django.db import models
 from users.models import User
-from ... import Title
+# from ... import Title
 
 
 class Review(models.Model):
     text = models.TextField()
     author = models.ForeignKey(
         User, on_delete=models.CASCADE, related_name='reviews')
-    title = models.ForeignKey(
-        Title, on_delete=models.CASCADE, related_name='reviews')
+    # title = models.ForeignKey(
+    #    Title, on_delete=models.CASCADE, related_name='reviews')
     score = models.IntegerField()
     pub_date = models.DateTimeField('Дата публикации', auto_now_add=True)
 
