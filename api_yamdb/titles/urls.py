3e03596d4709ae3aa3231935c04db46011d960f1
Igor Baryshev <igoralebar@gmail.com>
2023-03-03 18:09:15 +0300

    comment out djoser

--- api_yamdb/titles/urls.py
+++ api_yamdb/titles/urls.py
@@ -9,7 +9,7 @@ router.register('categories', CategoriesViewSet, basename='categories')
 
 
 urlpatterns = [
-    path('', include('djoser.urls')),
-    path('', include('djoser.urls.jwt')),
+    #path('', include('djoser.urls')),
+    #path('', include('djoser.urls.jwt')),
     path('', include(router.urls)),
 ]
