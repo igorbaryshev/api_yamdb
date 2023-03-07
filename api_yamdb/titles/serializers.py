411d9f191cb9545e88c0d637a0fd1ffb3d112d59
Евгений Субочев <panc96@yandex.ru>
2023-03-07 18:28:45 +0300

    Добавление вью, для всех моделей

--- api_yamdb/titles/serializers.py
+++ api_yamdb/titles/serializers.py
@@ -1,6 +1,5 @@
 from rest_framework import serializers
 
-
 from .models import Title, Genre, Category
 
 
@@ -9,3 +8,49 @@ class CategorySerializer(serializers.ModelSerializer):
     class Meta:
         fields = ['name', 'slug']
         model = Category
+
+
+class GenreSerializer(serializers.ModelSerializer):
+
+    class Meta:
+        fields = ['name', 'slug']
+        model = Genre
+
+
+class TitlesSerializer(serializers.ModelSerializer):
+    genre = GenreSerializer(many=True)
+    category = CategorySerializer()
+
+    class Meta:
+        model = Title
+        fields = ['id', 'name', 'year', 'description', 'genre', 'category']
+
+        
+
+class CreateTitleSerializer(serializers.ModelSerializer):
+    genre = serializers.SlugRelatedField(
+        queryset=Genre.objects.all(),
+        many=True,
+        slug_field='slug',
+    )
+    category = serializers.SlugRelatedField(
+        queryset=Category.objects.all(),
+        slug_field='slug'
+    )
+
+    class Meta:
+        model = Title
+        fields = ['id', 'name', 'year', 'description', 'category', 'genre']
+
+    def create(self, validated_data):
+        genres_data = validated_data.pop('genre')
+        category_data = validated_data.pop('category')
+        title = Title.objects.create(**validated_data, category=category_data)
+        for genre_data in genres_data:
+            genre = Genre.objects.get(slug=genre_data.slug)
+            title.genre.add(genre)
+        return title
+    
+    def to_representation(self, instance):
+        serializer = TitlesSerializer(instance)
+        return serializer.data
--- api_yamdb/titles/urls.py
+++ api_yamdb/titles/urls.py
@@ -1,12 +1,13 @@
 from django.urls import path, include
 from rest_framework.routers import SimpleRouter
 
-from .views import CategoriesViewSet
+from .views import CategoriesViewSet, GenresViewSet, TitlesViewSet
 
 
 router = SimpleRouter()
 router.register('categories', CategoriesViewSet, basename='categories')
-
+router.register('genres', GenresViewSet, basename='genres')
+router.register('titles', TitlesViewSet, basename='titles')
 
 urlpatterns = [
     path('', include('djoser.urls')),
